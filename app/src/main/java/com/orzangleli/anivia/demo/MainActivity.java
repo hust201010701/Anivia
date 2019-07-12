f8544204225b127233167129eda325617b56e091
lixiancheng <lixiancheng@zhuanzhuan.com>
2019-07-12 19:55:02 +0800

    支持在sd卡上调用dex文件

--- app/src/main/java/com/orzangleli/anivia/demo/MainActivity.java
+++ app/src/main/java/com/orzangleli/anivia/demo/MainActivity.java
@@ -1,7 +1,20 @@
 package com.orzangleli.anivia.demo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import dalvik.system.DexClassLoader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
     
@@ -10,5 +23,63 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     
         new Test().test("lxc");
+        
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.READ_EXTERNAL_STORAGE }, 0);
+            return ;
+        }
+    
+        patch();
+    }
+    
+    private void patch() {
+        try {
+            DexClassLoader classLoader = new DexClassLoader("/sdcard/patched.dex",this.getCacheDir().getAbsolutePath(),
+                null, MainActivity.class.getClassLoader());
+        
+            Class patchesInfoClass = classLoader.loadClass("com.orzangleli.anivia.patch.TestPatchable");
+            patchesInfoClass.newInstance();
+            Log.i("lxc", "patchesInfoClass ---> " + patchesInfoClass);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 0) {
+            patch();
+        }
+    }
+    
+    public void copy(String srcPath,String dstPath) throws IOException {
+        File src=new File(srcPath);
+        if(!src.exists()){
+            throw new RuntimeException("source patch does not exist ");
+        }
+        File dst=new File(dstPath);
+        if(!dst.getParentFile().exists()){
+            dst.getParentFile().mkdirs();
+        }
+        InputStream in = new FileInputStream(src);
+        try {
+            OutputStream out = new FileOutputStream(dst);
+            try {
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+            } finally {
+                out.close();
+            }
+        } finally {
+            in.close();
+        }
     }
 }
